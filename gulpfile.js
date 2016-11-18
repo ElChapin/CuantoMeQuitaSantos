b5766c57ed0187a607cab1001712555e355ab98f
Sebastián Rojas <sebastian@iridian.co>
2016-11-18 15:47:32 -0500

    esqueleto
    Quedó configurado con sass y gulp. Para ejecutar con livereload debe
    escribir gulp en la consola y ya

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var bower = require('bower');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var minifyCss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var sh = require('shelljs');
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+
+var paths = {
+  sass: ['./scss/**/*.scss']
+};
+
+gulp.task('sass', function(done) {
+  gulp.src('./scss/app.scss')
+    .pipe(sass())
+    .on('error', sass.logError)
+    .pipe(gulp.dest('./app/css/'))
+    .pipe(minifyCss({
+      keepSpecialComments: 0
+    }))
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(gulp.dest('./www/css/'))
+    .on('end', done);
+});
+
+gulp.task('sass-watch', function() {
+  gulp.watch(paths.sass, ['sass']);
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    root: 'app',
+    livereload: true
+  });
+});
+ 
+gulp.task('html', function () {
+  gulp.src('./app/*.html')
+    .pipe(connect.reload());
+});
+ 
+gulp.task('watch', function () {
+  gulp.watch(['./app/*.html', './app/css/*.css'], ['html']);
+});
+ 
+gulp.task('default', ['connect', 'watch', 'sass', 'sass-watch']);
