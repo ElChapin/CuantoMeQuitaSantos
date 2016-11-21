93e8079322966264075d4d0a971bdea27f37f162
Sebastián Rojas <sebastian@iridian.co>
2016-11-21 16:24:56 -0500

    Retefuente antes y con reforma

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -14,14 +14,94 @@ angular.module('myApp.view1', ['ngRoute'])
   var UVT = 29753;
   $scope.salarioMinimo = 689454;
 
-  $scope.salario = 1500000;
+  $scope.salario = 2500000;
 
   $scope.$watch('salario', function () {
     
-    calculoSalario()
+    calculoSalarioGeneral()
   })
 
-  var calculoSalario = function () {
+  var calcularRetencionActual = function (baseGravable) {
+
+    var baseGravableUVT = baseGravable / UVT;
+    var retencionUVT = 0;
+    //3.1.5.1: Cálculo de la retención en UVT según ARTICULO 383 del ET
+    /* Tomado el 19 de diciembre de 2013 de http://www.secretariasenado.gov.co/senado/basedoc/codigo/estatuto_tributario_pr016.html
+            
+            ARTICULO 383. TARIFA. <Artículo modificado por el artículo 23 de la Ley 1111 de 2006. El nuevo texto es el siguiente:>
+            <Inciso modificado por el artículo 13 de la Ley 1607 de 2012. El nuevo texto es el siguiente:> La retención en la fuente aplicable a los pagos gravables, efectuados por las personas naturales o jurídicas, las sociedades de hecho, las comunidades organizadas y las sucesiones ilíquidas, originados en la relación laboral, o legal y reglamentaria; efectuados a las personas naturales pertenecientes a la categoría de empleados de conformidad con lo establecido en el artículo 329 de este Estatuto; o los pagos recibidos por concepto de pensiones de jubilación, invalidez, vejez, de sobrevivientes y sobre riesgos laborales de conformidad con lo establecido en el artículo 206 de este Estatuto, será la que resulte de aplicar a dichos pagos la siguiente tabla de retención en la fuente:
+            
+            TABLA DE RETENCIÓN EN LA FUENTE PARA INGRESOS LABORALES GRAVADOS
+            
+            RANGOS EN UVT			TARIFA MARGINAL		IMPUESTO
+            DESDE	HASTA
+                  
+            >0		95				0%					0
+            >95		150				19%					(Ingreso laboral gravado expresado en UVT menos 95 UVT)*19%
+            >150	360				28%					(Ingreso laboral gravado expresado en UVT menos 150 UVT)*28% más 10 UVT
+            >360	En adelante		33%					(Ingreso laboral gravado expresado en UVT menos 360 UVT)*33% más 69 UVT
+            
+            PARÁGRAFO. Para efectos de la aplicación del Procedimiento 2 a que se refiere el artículo 386 de este Estatuto, el valor del impuesto en UVT determinado de conformidad con la tabla incluida en este artículo, se divide por el ingreso laboral total gravado convertido a UVT, con lo cual se obtiene la tarifa de retención aplicable al ingreso mensual.
+            */
+    if (baseGravableUVT > 360)
+        retencionUVT = (baseGravableUVT - 360) * .33 + 69;
+    else if (baseGravableUVT > 150)
+        retencionUVT = (baseGravableUVT - 150) * .28 + 10;
+    else if (baseGravableUVT > 95)
+        retencionUVT = (baseGravableUVT - 95) * .19;
+    return retencionUVT * UVT;
+  }
+
+  var calcularRetencionReforma = function (baseGravable) {
+
+    var baseGravableUVT = baseGravable / UVT;
+    var retencionUVT = 0;
+    //3.1.5.1: Cálculo de la retención en UVT según ARTICULO 383 del ET
+    /*
+    ARTÍCULO 16. Modifíquese el artículo 383 del Estatuto Tributario, el cual quedará así:
+    ARTICULO 383. TARIFA. La retención en la fuente aplicable a los pagos gravables
+    efectuados por las personas naturales o jurídicas, las sociedades de hecho, las
+    comunidades organizadas y las sucesiones ilíquidas, originados en la relación
+    laboral, o legal y reglamentaria, y los pagos recibidos por concepto de pensiones de
+    jubilación, invalidez, vejez, de sobrevivientes y sobre riesgos laborales de
+    conformidad con lo establecido en el artículo 206 de este Estatuto, será la que
+    resulte de aplicar a dichos pagos la siguiente tabla de retención en la fuente:
+    Rangos en UVT Tarifa
+    Marginal Retención
+    Desde Hasta
+    >0 50 0% 0
+    >50 83 10% (Base Gravable en UVT menos 50 UVT) x 10%
+    >83 166 20% (Base Gravable en UVT menos 83 UVT) x 20% + 4 UVT
+    >166 250 30% (Base Gravable en UVT menos 166 UVT) x 30% + 20
+    UVT
+    >250 333 33% (Base Gravable en UVT menos 250 UVT) x 33% + 45
+    UVT
+    >333 En
+    adelante 35% (Base Gravable en UVT menos 333 UVT) x 35% + 73
+    UVT
+
+    */ 
+    if (baseGravableUVT > 333)
+        retencionUVT = (baseGravableUVT - 333) * .35 + 73;
+    else if (baseGravableUVT > 250)
+        retencionUVT = (baseGravableUVT - 250) * .33 + 45;
+    else if (baseGravableUVT > 166)
+        retencionUVT = (baseGravableUVT - 166) * .3 + 20;
+    else if (baseGravableUVT > 83)
+        retencionUVT = (baseGravableUVT - 83) * .2 + 4;
+    else if (baseGravableUVT > 50)
+        retencionUVT = (baseGravableUVT - 50) * .1;
+        
+    return retencionUVT * UVT;
+  }
+
+  var calculoSalarioGeneral = function () {
+
+      $scope.actual = calculoSalario(calcularRetencionActual)
+      $scope.reforma = calculoSalario(calcularRetencionReforma)
+  }
+
+  var calculoSalario = function (calcularRetencion) {
 
     //1: Entradas
     
@@ -43,22 +123,22 @@ angular.module('myApp.view1', ['ngRoute'])
     var viaticos = 0;
     
     //1.3:Rentas excentas
-    $scope.aporteAFC = 0;
-    $scope.aporteFVP = 0;
+    var aporteAFC = 0;
+    var aporteFVP = 0;
 
-    if (($scope.aporteAFC + $scope.aporteFVP) > $scope.salario * .3) {
-        $scope.aporteAFC = 0;
-        $scope.aporteFVP = 0;
+    if ((aporteAFC + aporteFVP) > $scope.salario * .3) {
+        aporteAFC = 0;
+        aporteFVP = 0;
         alert('Los aportes al Fondo Voluntario de Pensiones (FVP) sumados a los aportes las Cuentas de Ahorro para el Fomento de la Construcción (AFC) no pueden superar el 30% del ingreso laboral (salario)');
     }
     
-    flujoEfectivoMensual -= $scope.aporteFVP + $scope.aporteAFC;
+    flujoEfectivoMensual -= aporteFVP + aporteAFC;
     
     //1.4:Deducciones
-    $scope.interesesOCorreccionMonetariaPrestamosVivienda = 0;
+    var interesesOCorreccionMonetariaPrestamosVivienda = 0;
     //medicina prepagada y pólizas de seguros
-    $scope.pagosSalud = 0;						
-    $scope.dependientes = false;
+    var pagosSalud = 0;						
+    var dependientes = false;
     
     //2: Seguridad social
     
@@ -86,7 +166,7 @@ angular.module('myApp.view1', ['ngRoute'])
         aporteFSP = IBC * .01;
     
     //Aporte del empleado (4%) más aporte del empleador (12%)
-    var totalAportePension = IBC * .16 + $scope.aporteFVP;
+    var totalAportePension = IBC * .16 + aporteFVP;
     
     flujoEfectivoMensual -= aporteObligatorioPension + aporteFSP;
     
@@ -117,13 +197,13 @@ angular.module('myApp.view1', ['ngRoute'])
     //3.1.3: Cálculos deducciones
     
     //3.1.3.1: Pagos salud
-    var pagosSaludUVT = $scope.pagosSalud / UVT;
-    var deduccionPagosSalud = (pagosSaludUVT > 16) ? 16 * UVT : $scope.pagosSalud;
+    var pagosSaludUVT = pagosSalud / UVT;
+    var deduccionPagosSalud = (pagosSaludUVT > 16) ? 16 * UVT : pagosSalud;
     
     //3.1.3.2: Dependientes
     var deduccionDependientes = 0;
 
-    if ($scope.dependientes) {
+    if (dependientes) {
 
       deduccionDependientes = $scope.salario * .1;
       var deduccionDependientesUVT = deduccionDependientes / UVT;
@@ -142,12 +222,12 @@ angular.module('myApp.view1', ['ngRoute'])
 
     //TODO: Cómo va ahí "Rentas de trabajo exentas numerales del 1 al 9 artículo 206 ET"
     //TODO: según el procedimeinto debería incluir o no el aporte FSP
-    var rentasExcentas = aporteObligatorioPension + $scope.aporteFSP + $scope.aporteFVP + $scope.aporteAFC;
+    var rentasExcentas = aporteObligatorioPension + aporteFSP + aporteFVP + aporteAFC;
       //TODO: según esto http://www.gerencie.com/disminucion-de-la-base-de-retencion-por-aportes-obligatorios-a-salud-y-pension.html y según el este artículo del estatuto tributario http://www.secretariasenado.gov.co/senado/basedoc/codigo/estatuto_tributario_pr006.html#126-1 debería sumarse a las rentas excentas el aporte a pension que hace el empleador
       //var rentasExcentas = totalAportePension + $scope.aporteFSP + $scope.aporteAFC;
     
     //TODO: dice Aportes obligatorios a salud efectuados por el trabajador (año anterior), pero es así en el procedimeinto 1?
-    var deducciones = aporteObligatorioSalud + $scope.interesesOCorreccionMonetariaPrestamosVivienda + deduccionPagosSalud + deduccionDependientes;
+    var deducciones = aporteObligatorioSalud + interesesOCorreccionMonetariaPrestamosVivienda + deduccionPagosSalud + deduccionDependientes;
     
     var subtotal = pagosLaborales - ingresosNoConstirutivosDeRenta - rentasExcentas - deducciones;
     
@@ -155,8 +235,7 @@ angular.module('myApp.view1', ['ngRoute'])
     var baseGravable = subtotal * .75;
     var retencion = calcularRetencion(baseGravable);
     
-    flujoEfectivoMensual -= retencion;
-    
+    flujoEfectivoMensual -= retencion;    
     
     //5. Flujo efectivo anual
     //=El flujo mensual, y si no es salario integral un salario de prima + 12% de intereses sobre cesantías (como son un salario, se suma 1.12 veces el salario)
@@ -166,47 +245,24 @@ angular.module('myApp.view1', ['ngRoute'])
     //=Un salario y 12% de intereses sobre la cesantía
     var cesantia = ($scope.salarioIntegral) ? 0 : $scope.salario;
     
-      //7. Mostrar salidas
-    form.find('[name="IBC"]').val(formatValueToCurency(IBC));
+    //7. Mostrar salidas
+    /*form.find('[name="IBC"]').val(formatValueToCurency(IBC));
     form.find('[name="aporteSalud"]').val(formatValueToCurency(aporteObligatorioSalud));
     form.find('[name="aportePension"]').val(formatValueToCurency(aporteObligatorioPension));
-    form.find('[name="aporteFSP"]').val(formatValueToCurency($scope.aporteFSP));
-    form.find('[name="seguridadSocial"]').val(formatValueToCurency(aporteObligatorioSalud + aporteObligatorioPension + $scope.aporteFSP));
+    form.find('[name="aporteFSP"]').val(formatValueToCurency(aporteFSP));
+    form.find('[name="seguridadSocial"]').val(formatValueToCurency(aporteObligatorioSalud +aporteObligatorioPension + aporteFSP));
     form.find('[name="baseGravable"]').val(formatValueToCurency(baseGravable));
     form.find('[name="retencion"]').val(formatValueToCurency(retencion));
     form.find('[name="aporteTotalPension"]').val(formatValueToCurency(totalAportePension));
-    form.find('[name="ahorroAnual"]').val(formatValueToCurency(cesantia + $scope.aporteAFC * 12));
+    form.find('[name="ahorroAnual"]').val(formatValueToCurency(cesantia + aporteAFC * 12));
     form.find('[name="flujoMensual"]').val(formatValueToCurency(flujoEfectivoMensual));
-    form.find('[name="flujoAnual"]').val(formatValueToCurency(flujoEfectivoAnual));
-  }
+    form.find('[name="flujoAnual"]').val(formatValueToCurency(flujoEfectivoAnual));*/
 
-  function calcularRetencion(baseGravable) {
-    var baseGravableUVT = baseGravable / UVT;
-    var retencionUVT = 0;
-    //3.1.5.1: Cálculo de la retención en UVT según ARTICULO 383 del ET
-    /* Tomado el 19 de diciembre de 2013 de http://www.secretariasenado.gov.co/senado/basedoc/codigo/estatuto_tributario_pr016.html
-            
-            ARTICULO 383. TARIFA. <Artículo modificado por el artículo 23 de la Ley 1111 de 2006. El nuevo texto es el siguiente:>
-            <Inciso modificado por el artículo 13 de la Ley 1607 de 2012. El nuevo texto es el siguiente:> La retención en la fuente aplicable a los pagos gravables, efectuados por las personas naturales o jurídicas, las sociedades de hecho, las comunidades organizadas y las sucesiones ilíquidas, originados en la relación laboral, o legal y reglamentaria; efectuados a las personas naturales pertenecientes a la categoría de empleados de conformidad con lo establecido en el artículo 329 de este Estatuto; o los pagos recibidos por concepto de pensiones de jubilación, invalidez, vejez, de sobrevivientes y sobre riesgos laborales de conformidad con lo establecido en el artículo 206 de este Estatuto, será la que resulte de aplicar a dichos pagos la siguiente tabla de retención en la fuente:
-            
-            TABLA DE RETENCIÓN EN LA FUENTE PARA INGRESOS LABORALES GRAVADOS
-            
-            RANGOS EN UVT			TARIFA MARGINAL		IMPUESTO
-            DESDE	HASTA
-                  
-            >0		95				0%					0
-            >95		150				19%					(Ingreso laboral gravado expresado en UVT menos 95 UVT)*19%
-            >150	360				28%					(Ingreso laboral gravado expresado en UVT menos 150 UVT)*28% más 10 UVT
-            >360	En adelante		33%					(Ingreso laboral gravado expresado en UVT menos 360 UVT)*33% más 69 UVT
-            
-            PARÁGRAFO. Para efectos de la aplicación del Procedimiento 2 a que se refiere el artículo 386 de este Estatuto, el valor del impuesto en UVT determinado de conformidad con la tabla incluida en este artículo, se divide por el ingreso laboral total gravado convertido a UVT, con lo cual se obtiene la tarifa de retención aplicable al ingreso mensual.
-            */
-    if (baseGravableUVT > 360)
-        retencionUVT = (baseGravableUVT - 360) * .33 + 69;
-    else if (baseGravableUVT > 150)
-        retencionUVT = (baseGravableUVT - 150) * .28 + 10;
-    else if (baseGravableUVT > 95)
-        retencionUVT = (baseGravableUVT - 95) * .19;
-    return retencionUVT * UVT;
-}
+    return {
+        pagosSeguridadSocial: aporteObligatorioSalud +aporteObligatorioPension + aporteFSP,
+        retefuente: retencion,
+        flujoMensual: flujoEfectivoMensual,
+        flujoAnual: flujoEfectivoAnual
+    }
+  }
 });
